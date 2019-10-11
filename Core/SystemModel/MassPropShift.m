90c7e561408f2a23a96dd423b1c478b8f88df332
Chris Regan <rega0051@umn.edu>
2019-10-11 12:31:59 -0500

    .

--- Core/SystemModel/MassPropShift.m
+++ Core/SystemModel/MassPropShift.m
@@ -1,14 +1,14 @@
-function I_B_kgm2 = MassPropShift(rCG_B_m, mass_kg, IRef_B_kgm2)
+function I_kgm2 = MassPropShift(rCG_m, mass_kg, IRef_kgm2)
 % OpenFlightAnalysis - MassPropShift
 %  Compute the Inertia due to a CG change.
 %
 % Inputs:
-%  rCG_B_m   - Mass CG shift distance
-%  mass_kg   - mass
-%  IRef_B_kgm2    - Reference Inertia
+%  rCG_m     - Mass CG shift distance
+%  mass_kg   - Mass [Kg]
+%  IRef_kgm2 - Reference Inertia
 %
 % Outputs:
-%  I_B_kgm2     - New Inertia Tensor
+%  I_kgm2    - Inertia taken about new location
 %
 
 % University of Minnesota
@@ -21,17 +21,17 @@ function I_B_kgm2 = MassPropShift(rCG_B_m, mass_kg, IRef_B_kgm2)
 %% Check I/O Arguments
 % Check the number of inputs
 narginchk(1, 3);
-if nargin < 3, IRef_B = [];
+if nargin < 3, IRef_kgm2 = [];
     if nargin < 2, mass_kg = []; end
 end
 
 % Default Values
 if isempty(mass_kg), mass_kg = 1.0; end
-if isempty(IRef_B_kgm2), IRef_B_kgm2 = zeros(3); end
+if isempty(IRef_kgm2), IRef_kgm2 = zeros(3); end
 
 % Check the number of outputs
 nargoutchk(0, 1);
 
 % Calculate the New Inertia % I_CG = I_Ref + m(dot(r, r)*I + (r*r')), where r is from Ref to CG
-I_B_kgm2 = IRef_B_kgm2 + mass_kg * (dot(rCG_B_m, rCG_B_m)*eye(3) + rCG_B_m * rCG_B_m');
+I_kgm2 = IRef_kgm2 + mass_kg * (dot(rCG_m, rCG_m)*eye(3) + rCG_m * rCG_m');
 
