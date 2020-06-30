f6a0346d88f61147b3cffb227e7f5773f266b242
Chris Regan <rega0051@umn.edu>
2020-06-29 20:13:35 -0500

    FFT power fix

--- Tests/TestFreqRespEstNoise.py
+++ Tests/TestFreqRespEstNoise.py
@@ -55,7 +55,7 @@ ampInit = 1
 ampFinal = ampInit
 freqMinDes_rps = 0.1 * hz2rps * np.ones(numExc)
 freqMaxDes_rps = 10 * hz2rps *  np.ones(numExc)
-freqStepDes_rps = (30 / freqRate_hz) * hz2rps
+freqStepDes_rps = (10 / freqRate_hz) * hz2rps
 methodSW = 'zip' # "zippered" component distribution
 
 # Generate MultiSine Frequencies
@@ -124,21 +124,22 @@ mDft, _ = FreqTrans.CZTMat(m, zPts)
 nDft, _ = FreqTrans.CZTMat(n, zPts)
 
 # Compute Power, factor of 2 because DFT is one-sided
-scale = 2 * FreqTrans.PowerScale('spectrum', np.ones_like(uExc), freqRate_hz)
+scale = FreqTrans.PowerScale('spectrum', np.ones_like(uExc), freqRate_hz)
 
-Puu = (uDft.conj() * uDft).real * scale
-Puz = (uDft.conj() * zDft) * scale
-Pzz = (zDft.conj() * zDft).real * scale
+Suu = (uDft.conj() * uDft).real * scale
+Suz = (uDft.conj() * zDft) * scale
+Szz = (zDft.conj() * zDft).real * scale
 
-Pyy = (yDft.conj() * yDft).real * scale
-Pmm = (mDft.conj() * mDft).real * scale
-Pnn = (nDft.conj() * nDft).real * scale
+Syy = (yDft.conj() * yDft).real * scale
+Smm = (mDft.conj() * mDft).real * scale
+Snn = (nDft.conj() * nDft).real * scale
 
 
 #%%
 
 # Nominal
-Tuz = Puz / Puu
+#Tuz = Suz / Suu
+Tuz = zDft / uDft
 
 gainTuz_mag, phaseTuz_deg = FreqTrans.GainPhase(Tuz, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
 gainTuz_dB = mag2db(gainTuz_mag)
@@ -149,89 +150,95 @@ uDftAccum = uDftHist.cumsum(axis = -1)
 zDftAccum = zDftHist.cumsum(axis = -1)
 
 scaleHist = scale * (N/np.arange(1, N+1))**2
-PuuHist = (uDftAccum.conj() * uDftAccum).real * scaleHist
-PuzHist = (uDftAccum.conj() * zDftAccum) * scaleHist
-PzzHist = (zDftAccum.conj() * zDftAccum).real * scaleHist
+SuuHist = (uDftAccum.conj() * uDftAccum).real * scaleHist
+SuzHist = (uDftAccum.conj() * zDftAccum) * scaleHist
+SzzHist = (zDftAccum.conj() * zDftAccum).real * scaleHist
 
 #plt.figure
 #plt.subplot(2,1,1); plt.grid(True)
-#plt.plot(mag2db(PuuHist)[0].T)
+#plt.plot(mag2db(SuuHist)[0].T)
 #plt.subplot(2,1,2); plt.grid(True)
-#plt.plot(mag2db(PzzHist)[0].T)
+#plt.plot(mag2db(SzzHist)[0].T)
 
 
-PuuVar = PuuHist.var(axis = -1)
-PzzVar = PzzHist.var(axis = -1)
+SuuVar = SuuHist.var(axis = -1)
+SzzVar = SzzHist.var(axis = -1)
 
 #plt.figure
 #plt.subplot(2,1,1); plt.grid(True)
-#plt.plot(freqChan_rps[0], mag2db(PuuVar).T)
+#plt.plot(freqChan_rps[0], mag2db(SuuVar).T)
 #plt.subplot(2,1,2); plt.grid(True)
-#plt.plot(freqChan_rps[0], mag2db(PzzVar).T)
+#plt.plot(freqChan_rps[0], mag2db(SzzVar).T)
 
 
 ## Null Technique
 freq = freqChan_rps[0]
 freq_N = freqGap_rps
 
-uDft_N, uDftHist_N = FreqTrans.CZTMat(uExc, zPtsNull)
-zDft_N, zDftHist_N = FreqTrans.CZTMat(z, zPtsNull)
+uDftNull_N, uDftNullHist_N = FreqTrans.CZTMat(uExc, zPtsNull)
+nDft_N, nDftHist_N = FreqTrans.CZTMat(z, zPtsNull) # z at Null is n
 
 #
-PuuNull_N = (uDft_N.conj() * uDft_N).real * scale
-PuuNull_E = FreqTrans.InterpPolar(PuuNull_N, freq_N, freq, 'linear')
+SuuNull_N = (uDftNull_N.conj() * uDftNull_N).real * scale
+SuuNull_E = FreqTrans.InterpPolar(SuuNull_N, freq_N, freq, 'linear')
 
-uDftNull_E = FreqTrans.InterpPolar(uDft_N, freq_N, freq, 'linear')
-PuuNull_E1 = (uDftNull_E.conj() * uDftNull_E).real * scale
+uDftNull_E = FreqTrans.InterpPolar(uDftNull_N, freq_N, freq, 'linear')
+SuuNull_E1 = (uDftNull_E.conj() * uDftNull_E).real * scale
 
 # 
-PzzNull_N = (zDft_N.conj() * zDft_N).real * scale
-PzzNull_E = FreqTrans.InterpVal(PzzNull_N, freq_N, freq, 'linear')
+Snn_N = (nDft_N.conj() * nDft_N).real * scale
+Snn_E = FreqTrans.InterpVal(Snn_N, freq_N, freq, 'linear')
 
-
-zDftNull_E = FreqTrans.InterpPolar(zDft_N, freq_N, freq, 'linear')
-PzzNull_E1 = (zDftNull_E.conj() * zDftNull_E).real * scale
+nDft = FreqTrans.InterpPolar(nDft_N, freq_N, freq, 'linear')
+Snn_E1 = (nDft.conj() * nDft).real * scale
 
 
 # Null Cross Spectrum
 numFreqNull = len(freq_N)
-uDftOnes_N = np.ones_like(zDft_N) / (numFreqNull * scale)**0.5
-PuuOnes_N = (uDftOnes_N.conj() * uDftOnes_N).real * scale
+#uDftOnes_N = np.ones_like(nDft_N) / (numFreqNull * scale)**0.5
+#SuuOnes_N = (uDftOnes_N.conj() * uDftOnes_N).real * scale
 
-PuzNull_N = uDft_N.conj() * zDft_N * scale
-Puu_N = uDft_N.conj() * uDft_N * scale
-TuzNull_N = PuzNull_N / Puu_N
+#SuzNull_N = uDftNull_N.conj() * nDft_N * scale
+#SuuNull_N = uDftNull_N.conj() * uDftNull_N * scale
+#TuzNull_N = SuzNull_N / SuuNull_N
 
-Puz_N = uDft.conj() * FreqTrans.InterpPolar(zDft_N, freq_N, freq, 'linear') * scale
-
-
-TuzUnc = FreqTrans.InterpPolar(uDft_N, freq_N, freq, 'linear').conj() / zDft
-TuzUnc_N = uDft_N.conj() / FreqTrans.InterpPolar(zDft, freq, freq_N, 'linear')
+Sun = uDft.conj() * nDft * scale
 
 
-#Pxx = xDft.conj() * xDft * scale = (xR - xI) * (xR + xI) * scale
-#Pyy = yDft.conj() * yDft * scale = (yR - yI) * (yR + yI) * scale
+TuzUnc = nDft / uDft
+TuzUnc_N = nDft_N / FreqTrans.InterpPolar(uDft, freq, freq_N, 'linear')
+
+#Sxx = xDft.conj() * xDft * scale = (xR - xI) * (xR + xI) * scale
+#Syy = yDft.conj() * yDft * scale = (yR - yI) * (yR + yI) * scale
 #
-#Pxy = xDft.conj() * yDft * scale = (xR - xI) * (yR + yI) * scale
-#Txy = Pxy / Pxx = [xDft.conj() * yDft] / [xDft.conj() * xDft]
+#Sxy = xDft.conj() * yDft * scale = (xR - xI) * (yR + yI) * scale
+#Txy = Sxy / Sxx = [xDft.conj() * yDft] / [xDft.conj() * xDft]
 #    = [(xR - xI) * (yR + yI)] / [(xR - xI) * (xR + xI)] = (yR + yI) / (xR + xI)
 #    = yDft / xDft
 
-#Cxy = |Pxy|^2 / [Pxx * Pyy]
+#Cxy = |Sxy|^2 / [Sxx * Syy]
+#    = [Sxy.conj() * Sxy] / [Sxx * Syy]
+#    = [(xR + xI) * (yR - yI) * (xR - xI) * (yR + yI)] / [(xR - xI) * (xR + xI) * (yR - yI) * (yR + yI)]
+#    = 1 # Always =1 by definition, some alteration of Sxy, Sxx, or Szz required!!
 
 #Cxy = 1, then
-#Pyy = |Pxy|^2 / Pxx
-#|Pxy|^2 = [Pxx * Pyy]
-# Txy = Pxy / Pxx
-# |Txy| = |Pxy| / |Pxx|
+#Syy = |Sxy|^2 / Sxx = |Txy * Sxx|^2 / Sxx
+# Sxx is real and >= 0, so: Syy = |Txy|^2 * Sxx
 
-gainTuzNull_mag, phaseTuzNull_deg = FreqTrans.GainPhase(TuzNull_N, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
-gainTuzNull_dB = mag2db(gainTuzNull_mag)
+# Perturbed
+# Syy_p = Syy + Snn
+#       = |Sxy|^2 / Sxx + |Sxn|^2 / Sxx # Additive Output
+#       = |Txy|^2 * Sxx + |Txn|^2 * Sxx # Additive Output
+# Then: Txn = Sxn / Sxx = nDft / xDft
 
+# Syy_p = Syy + Snn
+#       = |Sxy|^2 / Sxx + |Syn|^2 / Syy # Multiplicative Output
+#       = |Txy|^2 * Sxx + |Tyn|^2 * Syy # Multiplicative Output
+# Then: Tyn = Syn / Syy = nDft / yDft
 
 #%%
 #%% Estimate the frequency response function
-optSpec = FreqTrans.OptSpect(dftType = 'czt', freqRate = freqRate_rps, scaleType = 'spectrum', smooth = ('box', 3), winType = ('tukey', 0.1), detrendType = 'Linear')
+optSpec = FreqTrans.OptSpect(dftType = 'czt', freqRate = freqRate_rps, scaleType = 'spectrum', smooth = ('box', 5), winType = ('tukey', 0.0), detrendType = 'Linear')
 
 # Excited Frequencies per input channel
 optSpec.freq = freqChan_rps
@@ -242,8 +249,8 @@ optSpec.freqNull = freqGap_rps
 optSpec.freqNullInterp = True
 
 # FRF Estimate
-#freq_rps, Txy, Cxy, Pxx, Pyy, Pxy = FreqTrans.FreqRespFuncEst(uExc, z, optSpec)
-freq_Func_rps, Tuz_Func, Cuz_Func, Puu_Func, Pzz_Func, Puz_Func, TuzUnc_Func, PuuNull_Func, PzzNull_Func = FreqTrans.FreqRespFuncEstNoise(uExc, z, optSpec)
+#freq_rps, Txy, Cxy, Sxx, Syy, Sxy = FreqTrans.FreqRespFuncEst(uExc, z, optSpec)
+freq_Func_rps, Tuz_Func, Cuz_Func, Suu_Func, Szz_Func, Suz_Func, TuzUnc_Func, SuuNull_Func, Snn_Func = FreqTrans.FreqRespFuncEstNoise(uExc, z, optSpec)
 freq_hz = freq_Func_rps * rps2hz
 
 T_Func = Tuz_Func
@@ -267,72 +274,86 @@ freqLin_hz = freqLin_rps * rps2hz
 TuyLinReal, TuyLinImag, _ = control.nyquist(sysPlant, omega = freqLin_rps, Plot = False)
 TuyLin = TuyLinReal + 1j * TuyLinImag
 
-TmzLinReal, TmzLinImag, _ = control.nyquist(sysNoise, omega = freqLin_rps, Plot = False)
-TmzLin = TmzLinReal + 1j * TmzLinImag
+TmnLinReal, TmnLinImag, _ = control.nyquist(sysNoise, omega = freqLin_rps, Plot = False)
+TmnLin = TmnLinReal + 1j * TmnLinImag
 
 # Just the plant
 numFreqLin = len(freqLin_rps)
-PuuLin = np.ones_like(TuyLin, dtype = float) * Puu.sum() / len(freqLin_hz)
-PyyLin = np.abs(TuyLin)**2 * PuuLin
+SuuLin = np.ones_like(TuyLin, dtype = float) * Suu.sum() / len(freqLin_hz)
+SyyLin = np.abs(TuyLin)**2 * SuuLin
+SuyLin = TuyLin * SuuLin
 
-# Just the noise model
-PmmLin = np.ones_like(TmzLin, dtype = float) * Pmm.sum() / len(freqLin_hz)
-PnnLin = np.abs(TmzLin)**2 * PmmLin
+# Just the noise model, this is an exogenous addition!!
+SmmLin = np.ones_like(TmnLin, dtype = float) * Smm.sum() / len(freqLin_hz)
+SnnLin = np.abs(TmnLin)**2 * SmmLin
+SmnLin = TmnLin * SmmLin
 
-PzzLin = PyyLin + PnnLin
 
-gainTmzLin_mag, phaseTmzLin_deg = FreqTrans.GainPhase(TmzLin, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
-gainTmzLin_dB = mag2db(gainTmzLin_mag)
+TunLin = (TmnLin * SmmLin) / SuuLin # Additive Output Equivalance
+TynLin = (TmnLin * SmmLin) / SyyLin # Multiplicative Output Equivalance
+
+SzzLin = SyyLin + SnnLin
+
 gainTuyLin_mag, phaseTuyLin_deg = FreqTrans.GainPhase(TuyLin, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
 gainTuyLin_dB = mag2db(gainTuyLin_mag)
+gainTmnLin_mag, phaseTmnLin_deg = FreqTrans.GainPhase(TmnLin, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
+gainTmnLin_dB = mag2db(gainTmnLin_mag)
+gainTunLin_mag, phaseTunLin_deg = FreqTrans.GainPhase(TunLin, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
+gainTunLin_dB = mag2db(gainTunLin_mag)
+gainTynLin_mag, phaseTynLin_deg = FreqTrans.GainPhase(TynLin, magUnit = 'mag', phaseUnit = 'deg', unwrap = True)
+gainTynLin_dB = mag2db(gainTynLin_mag)
 
 
 #%%
 plt.figure('Power via Null')
-plt.subplot(2,1,1); plt.grid(True)
-plt.semilogx(freqLin_hz, mag2db(PuuLin), '.k-', label = 'Linear Plant')
-plt.semilogx(freq * rps2hz, mag2db(Puu_Func[0]), '*b-', label = 'Estimate at Excitation')
-plt.semilogx(freq * rps2hz, mag2db(PuuNull_Func[0]), '.m--', label = 'Estimate of Null')
+plt.subplot(2,2,1); plt.grid(True)
+plt.semilogx(freqLin_hz, mag2db(SuuLin), '.k-', label = 'Linear Plant')
+#plt.semilogx(freq * rps2hz, mag2db(Suu[0]), '*b-', label = 'Estimate at Excitation XX')
+plt.semilogx(freq * rps2hz, mag2db(Suu_Func[0]), '*b-', label = 'Estimate')
 plt.legend()
-plt.ylabel('Input Power (dB)')
+plt.ylabel('Spectra (dB)')
+plt.title('Input at Excitation Set')
 
-plt.subplot(2,1,2); plt.grid(True)
-plt.semilogx(freqLin_hz, mag2db(PyyLin), '.k-', label = 'Linear Plant')
-plt.semilogx(freqLin_hz, mag2db(PnnLin), '*k--', label = 'Linear Noise ')
-plt.semilogx(freqLin_hz, mag2db(PzzLin), '*k-.', label = 'Linear System')
-plt.semilogx(freq * rps2hz, mag2db(Pzz_Func[0]), '*b-', label = 'Estimate at Excitation')
-plt.semilogx(freq * rps2hz, mag2db(PzzNull_Func[0]), '.m--', label = 'Estimate of Null')
+plt.subplot(2,2,2); plt.grid(True)
+plt.semilogx(freqLin_hz, mag2db(SyyLin), '.k-', label = 'Linear Plant')
+plt.semilogx(freqLin_hz, mag2db(SzzLin), '*k-.', label = 'Linear System')
+#plt.semilogx(freq * rps2hz, mag2db(Szz[0]), '*g-.', label = 'Estimate at Excitation XX')
+plt.semilogx(freq * rps2hz, mag2db(Szz_Func[0]), '*b-', label = 'Estimate')
 ylim = plt.ylim()
 plt.legend()
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Output Power (dB)')
+#plt.ylabel('Spectra (dB)')
+plt.title('Output at Excitation Set')
 
-#plt.subplot(3,1,3); plt.grid(True)
-#plt.semilogx(freqLin_hz, mag2db(PyyLin), '.k-', label = 'Output (Linear Plant Model)')
-#plt.semilogx(freqLin_hz, mag2db(PzzLin), '*k-', label = 'Output (Linear Model)')
-#
-#PzzErrRatio = PzzNull_Func[0] / Pzz_Func[0]
-#PzzErrMin = 1 - PzzErrRatio
-#PzzErrMin_db = -mag2db(PzzErrMin)
-#PzzErrMin_db[np.isnan(PzzErrMin_db)] = 1e6
-#
-#PzzErrMax = 1 + PzzErrRatio
-#PzzErrMax_db = mag2db(PzzErrMax)
-#
-#PzzErr_db = [PzzErrMin_db, PzzErrMax_db]
-#
-#plt.errorbar(freq * rps2hz, mag2db(Pzz_Func[0].T), yerr = PzzErr_db, fmt = '*b-', label = 'Output (Estimate with Uncertainty)')
-#plt.ylim(ylim)
-#plt.legend();
+plt.subplot(2,2,3); plt.grid(True)
+#plt.semilogx(freq_N * rps2hz, mag2db(SuuNull_N[0]), '.m--', label = 'Estimate of Null XX')
+plt.semilogx(freq * rps2hz, mag2db(SuuNull_Func[0]), '.m--', label = 'Estimate')
+plt.semilogx(freqLin_hz, mag2db(np.ones_like(freqLin_hz)/N**2), '-k', label = 'DFT Accuracy')
+plt.legend()
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Spectra (dB)')
+plt.title('Input at Null Set')
+
+plt.subplot(2,2,4); plt.grid(True)
+plt.semilogx(freqLin_hz, mag2db(SnnLin), '*k--', label = 'Linear Noise')
+#plt.semilogx(freq_N * rps2hz, mag2db(Snn_N[0]), '*g-.', label = 'Estimate of Null XX')
+#plt.semilogx(freq * rps2hz, mag2db(Snn[0]), '*m-.', label = 'Estimate of Null XX')
+plt.semilogx(freq * rps2hz, mag2db(Snn_Func[0]), '.m--', label = 'Estimatel')
+plt.legend()
+plt.xlabel('Frequency (Hz)')
+#plt.ylabel('Spectra (dB)')
+plt.title('Output at Null Set')
+
 
 #%%
 plt.figure('Gain')
 plt.subplot(3,1,1)
 plt.semilogx(freqLin_rps     * rps2hz, gainTuyLin_dB, '.k-', label = 'Linear Plant')
-#plt.semilogx(freqLin_rps     * rps2hz, gainTuzLin_dB, '*k--', label = 'Nominal (Linear System )')
-#plt.semilogx(freqLin_rps * rps2hz, gainTmzLin_dB, '*k-.', label = 'Noise (Linear System)')
-#plt.semilogx(freqChan_rps[0] * rps2hz, gain_Func_db[0], '*b-', label = 'Nominal (Estimate)')
-#plt.semilogx(freq * rps2hz, gainUnc_Func_dB[0], '.g-', label = 'Noise (Noise Estimate)')
+#plt.semilogx(freqLin_rps     * rps2hz, gainTuzLin_dB, '*k--', label = 'Linear System ')
+#plt.semilogx(freqLin_rps * rps2hz, gainTmnLin_dB, '*k-.', label = 'Linear Noise (Exogenous)')
+plt.semilogx(freqLin_rps * rps2hz, gainTunLin_dB, '*k-.', label = 'Linear Noise (Additive)')
+#plt.semilogx(freqLin_rps * rps2hz, gainTynLin_dB, '*k-.', label = 'Linear Noise (Multiplicitive)')
+plt.semilogx(freq * rps2hz, gain_Func_db[0], '*b-', label = 'Estimate')
+plt.semilogx(freq * rps2hz, gainUnc_Func_dB[0], '.g-', label = 'Noise Estimate (Additive)')
 ylim = plt.ylim()
 
 gainErrRatio = gainUnc_Func_mag[0] / gain_Func_mag[0]
@@ -345,8 +366,8 @@ gainErrMax_db = mag2db(gainErrMax)
 
 gainErr_db = [gainErrMin_db, gainErrMax_db]
 
-plt.semilogx(freq * rps2hz, mag2db(gainUnc_Func_mag[0]), '.m-', label = 'Noise Estimate')
-plt.errorbar(freq * rps2hz, gain_Func_db[0].T, yerr = gainErr_db, fmt = '*b-', label = 'Estimate with Uncertainty')
+#plt.semilogx(freq * rps2hz, mag2db(gainUnc_Func_mag[0]), '.m-', label = 'Noise Estimate')
+#plt.errorbar(freq * rps2hz, gain_Func_db[0].T, yerr = gainErr_db, fmt = '*b-', label = 'Estimate with Uncertainty')
 
 
 plt.ylim(ylim)
@@ -366,12 +387,12 @@ plt.ylim(0, 1.2)
 plt.grid(True);
 
 #%%
-fig = 'Bode'
-fig = FreqTrans.PlotBode(freqLin_rps * rps2hz, gainTuyLin_mag, phaseTuyLin_deg, coher_nd = np.ones_like(gainTuyLin_mag), gainUnc_mag = None, fig = fig, fmt = 'k', label='Linear Plant', dB = True)
-fig = FreqTrans.PlotBode(freq * rps2hz, gain_Func_mag[0], phase_Func_deg[0], coher_nd = Cuz_Func[0], gainUnc_mag = gainUnc_Func_mag[0], fig = fig, fmt = '*b-', label='Estimate', dB = True)
-
-#fig = FreqTrans.PlotBode(freq * rps2hz, gainUnc_Func_mag[0], phase_Func_deg[0], coher_nd = Cuz_Func[0], gainUnc_mag = gainUnc_Func_mag[0], fig = fig, fmt = '*b-', label='Estimate', dB = True)
-
-ax = fig.get_axes()
-ax[-1].set_ylim(0, 1.2)
+#fig = 'Bode'
+#fig = FreqTrans.PlotBode(freqLin_rps * rps2hz, gainTuyLin_mag, phaseTuyLin_deg, coher_nd = np.ones_like(gainTuyLin_mag), gainUnc_mag = None, fig = fig, fmt = 'k', label='Linear Plant', dB = True)
+#fig = FreqTrans.PlotBode(freq * rps2hz, gain_Func_mag[0], phase_Func_deg[0], coher_nd = Cuz_Func[0], gainUnc_mag = gainUnc_Func_mag[0], fig = fig, fmt = '*b-', label='Estimate', dB = True)
+#
+##fig = FreqTrans.PlotBode(freq * rps2hz, gainUnc_Func_mag[0], phase_Func_deg[0], coher_nd = Cuz_Func[0], gainUnc_mag = gainUnc_Func_mag[0], fig = fig, fmt = '*b-', label='Estimate', dB = True)
+#
+#ax = fig.get_axes()
+#ax[-1].set_ylim(0, 1.2)
 
